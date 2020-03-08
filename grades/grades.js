d88c02a274723ab7497043bb9a6feebf5e2333a9
zhu-tom <zhutom01@gmail.com>
2020-03-08 15:24:12 -0400

    Create grades.js

---
+++ grades/grades.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('grades').prepend('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>');
+    $.ajax();
+});
